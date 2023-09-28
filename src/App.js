6b4ad5da42eb7f8eb61b4e5a41e84c58c5413542
MercyAngelin2000 <mercyangelin2000@gmail.com>
2023-09-28 13:47:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import axios from "axios";
 
 function App() {
+  const [boyName, setBoyName] = useState(null)
+  const [girlName, setGirlName] = useState(null)
+  const [result, setResult] = useState(null)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const userData = {
+      boyName: boyName,
+      girlName: girlName
+    };
+    axios.post("http://127.0.0.1:8000/check", userData).then((response) => {
+      setResult(response)
+    });
+  }
+  const handleClear = () => {
+    setBoyName(null)
+    setGirlName(null)
+    setResult(null)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <div class="container">
+        <div class="d-flex justify-content-center align-items-center min vh-100 " style={{ fontFamily: "cursive" }}>
+          <div class="col-md-6 " >
+            <div className="card custum_card" >
+              <div className='card-header'><h5 style={{color:"#a40b9c"}}>Together Forever</h5></div>
+              <div className="card-body col d-flex justify-content-center">
+                <form onSubmit={handleSubmit} className='field'>
+                  <div className="row p-3">
+                    <div className="col-lg-4">
+                      <label>Boy Name</label>
+                    </div>
+                    <div className="col-lg-8">
+                      <input type="text" className="form-control" placeholder="Enter boy name" value={boyName} onChange={(e) => setBoyName(e?.target?.value)} />
+                    </div>
+                  </div>
+                  <div className="row p-3">
+                    <div className="col-lg-4">
+                      <label>Girl Name</label>
+                    </div>
+                    <div className="col-lg-8">
+                      <input type="text" className="form-control" placeholder="Enter girl name" value={girlName} onChange={(e) => setGirlName(e?.target?.value)} />
+                    </div>
+                  </div>
+                  <div className="row float-right p-3">
+                    <button type="submit" className="btn btn-theme mr-3" disabled={boyName && girlName ? false : true}>Submit</button>
+                    <button type="reset" className="btn btn-cancel" onClick={() => handleClear()}>Clear</button>
+                  </div>
+                </form>
+              </div>
+              {
+                result &&
+                <div className='card-footer'>
+                  <h5 style={{color:"#a40b9c"}}>{result?.data?.res}</h5>
+                </div>
+              }
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
