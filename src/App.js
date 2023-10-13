4d34a71aeb540f38aa99d7dc13fb0cc628489e67
MercyAngelin2000 <mercyangelin2000@gmail.com>
2023-10-13 14:28:06 +0530

    text added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,78 @@
 import './App.css';
 import React, { useState } from 'react';
-import axios from "axios";
+// import axios from "axios";
 
 function App() {
   const [boyName, setBoyName] = useState(null)
   const [girlName, setGirlName] = useState(null)
   const [result, setResult] = useState(null)
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const userData = {
+  //     boyName: boyName,
+  //     girlName: girlName
+  //   };
+  //   setLoading(true)
+  //   // http://127.0.0.1:8000/check
+  //   // https://flamesappbackend.onrender.com/check
+  //   axios.post("https://flamesappbackend.onrender.com/check", userData).then((response) => {
+  //     setLoading(false)
+  //     setResult(response)
+  //   });
+  // }
   const handleSubmit = (e) => {
-    e.preventDefault();
-    const userData = {
-      boyName: boyName,
-      girlName: girlName
-    };
-    axios.post("https://flamesappbackend.onrender.com/check", userData).then((response) => {
-      setResult(response)
-    });
+    e?.preventDefault()
+    var flames = ['F', 'L', 'A', 'M', 'E', 'S'];
+    const boy = boyName.toLowerCase().replace(/ /g, '').split('');
+    const girl = girlName.toLowerCase().replace(/ /g, '').split('');
+    const boyIndex = [];
+    const girlIndex = [];
+    for (let i = 0; i < boy.length; i++) {
+      for (let j = 0; j < girl.length; j++) {
+        if (boy[i] === girl[j]) {
+          boyIndex.push(i);
+          girlIndex.push(j);
+          boy[i] = 0;
+          girl[j] = 0;
+          break;
+        }
+      }
+    }
+    const overallLength = (boy.length - boyIndex.length) + (girl.length - girlIndex.length);
+    for (let letter = 0; letter < 5; letter++) {
+      const temp = [];
+      let x = overallLength;
+      if (overallLength > flames?.length) {
+        x %= flames?.length;
+        if(x===0){
+          temp?.push(...flames?.pop())
+        }
+        else{
+          temp?.push(...flames?.slice(x, flames.length), ...flames?.slice(0, x - 1));
+        }
+      } else {
+        temp?.push(...flames?.slice(x, flames.length), ...flames?.slice(0, x - 1));
+      }
+      flames.length = 0;
+      flames?.push(...temp);
+    }
+
+    flames = flames.join('');
+    let out = '';
+    if (flames === 'F') {
+      out = 'Friend';
+    } else if (flames === 'L') {
+      out = 'Love';
+    } else if (flames === 'A') {
+      out = 'Affection';
+    } else if (flames === 'M') {
+      out = 'Marriage';
+    } else if (flames === 'E') {
+      out = 'Enemy';
+    } else if (flames === 'S') {
+      out = 'Sister';
+    }
+    setResult(`The relationship between ${boyName} and ${girlName} is ${out}`);
   }
   const handleClear = () => {
     setBoyName(null)
@@ -24,11 +82,14 @@ function App() {
   return (
     <div className="App" >
       <div class="container">
-        <div class="d-flex justify-content-center align-items-center min vh-100 " style={{ fontFamily: "cursive" }}>
+        <div className='row head'>
+          <h3>A perfect relationship is not perfect, It's just that both people never gave up...</h3>
+        </div>
+        <div class="d-flex justify-content-center align-items-center" style={{ fontFamily: "cursive" }}>
           <div class="col-md-6 " >
             <div className="card custum_card" >
-              <div className='card-header'><h5 style={{color:"#a40b9c"}}>Together Forever</h5></div>
-              <div className="card-body col d-flex justify-content-center">
+              <div className='card-header'><h5 style={{ color: "#a40b9c" }}>Together Forever</h5></div>
+              <div className="card-body col d-flex justify-content-center">               
                 <form onSubmit={handleSubmit} className='field'>
                   <div className="row p-3">
                     <div className="col-lg-4">
@@ -55,7 +116,7 @@ function App() {
               {
                 result &&
                 <div className='card-footer'>
-                  <h5 style={{color:"#a40b9c"}}>{result?.data?.res}</h5>
+                  <h5 style={{ color: "#a40b9c" }}>{result}</h5>
                 </div>
               }
             </div>
